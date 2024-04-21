ec1c0d619d10e2557f0b91c3b0b7229334ecf111
Psamcyite <amojoayokunle.com>
2024-04-21 08:43:16 -0500

    added Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
 import Roadmap from "./components/Roadmap";
 import Services from "./components/Services";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
       </div>
 
       <ButtonGradient />
+      <Analytics />
     </>
   );
 };
